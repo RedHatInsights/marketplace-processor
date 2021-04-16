a7964b0b6c3ab09fe4ae16a6cc1b94c1e37e27e9
Gabor Burges <gburges@redhat.com>
2021-04-16 19:31:48 +0200

    Updating pr check for bonfire release

--- pr_check.sh
+++ pr_check.sh
@@ -11,13 +11,10 @@ IQE_PLUGINS="marketplace"
 IQE_MARKER_EXPRESSION="smoke"
 IQE_FILTER_EXPRESSION=""
 
-echo "LABEL quay.expires-after=3d" >> ./Dockerfile # tag expire in 3 days
-
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-curl -s $CICD_URL/bootstrap.sh -o bootstrap.sh
-source bootstrap.sh  # checks out bonfire and changes to "cicd" dir...
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
-source build.sh
-source deploy_ephemeral_env.sh
-source smoke_test.sh
+source $CICD_ROOT/build.sh
+source $CICD_ROOT/deploy_ephemeral_env.sh
+source $CICD_ROOT/smoke_test.sh
