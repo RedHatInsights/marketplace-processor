f7a24141ba18638aa3c48eaed39d9a98dcf2095f
Chris Hambridge <chambrid@redhat.com>
2021-08-02 14:50:19 -0400

    Improve readiness checking (#80)
    * Add readiness checks for kafka and db
    * Add liveliness check

--- marketplace/api/status/view.py
+++ marketplace/api/status/view.py
@@ -15,6 +15,16 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 """View for server status."""
+import logging
+import socket
+from http import HTTPStatus
+
+from django.db import connection
+from django.db import InterfaceError
+from django.db import NotSupportedError
+from django.db import OperationalError
+from django.db import ProgrammingError
+from kafka import BrokerConnection
 from rest_framework import permissions
 from rest_framework.decorators import api_view
 from rest_framework.decorators import permission_classes
@@ -22,12 +32,64 @@ from rest_framework.response import Response
 
 from api.status.model import Status
 from api.status.serializer import StatusSerializer
+from config.settings.base import INSIGHTS_KAFKA_HOST
+from config.settings.base import INSIGHTS_KAFKA_PORT
+
+
+LOG = logging.getLogger(__name__)
+BROKER_CONNECTION = BrokerConnection(INSIGHTS_KAFKA_HOST, int(INSIGHTS_KAFKA_PORT), socket.AF_UNSPEC)
+
+
+def check_kafka_connection():
+    """Check connectability of Kafka Broker."""
+    connected = BROKER_CONNECTION.connect_blocking(timeout=1)
+    if connected:
+        BROKER_CONNECTION.close()
+    return connected
+
+
+def check_database_connection():
+    """Collect database connection information.
+    :returns: boolean
+    """
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT datname AS database,
+                    numbackends as database_connections
+                FROM pg_stat_database
+                """
+            )
+            raw = cursor.fetchall()
+
+            # get pg_stat_database column names
+            names = [desc[0] for desc in cursor.description]
+    except (InterfaceError, NotSupportedError, OperationalError, ProgrammingError) as exc:
+        LOG.warning("Unable to connect to DB: %s", str(exc))
+        return False
+
+    # transform list-of-lists into list-of-dicts including column names.
+    result = [dict(zip(names, row)) for row in raw]
+
+    return True
 
 
 @api_view(["GET", "HEAD"])
 @permission_classes((permissions.AllowAny,))
 def status(request):
     """Provide the server status information."""
+    if "liveness" in request.query_params:
+        return Response({"alive": True})
+
+    if not check_kafka_connection():
+        status = HTTPStatus.FAILED_DEPENDENCY
+        return Response(data={"error": "Unable to establish connection with broker.", "status": status}, status=status)
+
+    if not check_database_connection():
+        status = HTTPStatus.FAILED_DEPENDENCY
+        return Response(data={"error": "Database backend not connected.", "status": status}, status=status)
+
     status_info = Status()
     serializer = StatusSerializer(status_info)
     server_info = serializer.data
